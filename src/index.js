8cc985569b90e8b3c9bc576497f736b05af20c1a
baky30 <amanbakiev99@gmail.com>
2023-06-14 19:47:26 +0400

    update react, react-dom; replace node-sass with sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { routerMiddleware } from "connected-react-router";
 import {createStore, applyMiddleware, compose} from 'redux';
 import { Provider } from 'react-redux';
@@ -40,11 +40,11 @@ export const store = createStore(
 
 store.dispatch(doInit());
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
